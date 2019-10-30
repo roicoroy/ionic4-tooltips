fb92dd5c659aaf9441696f696993a74c4463e1d7
Jeremy Quick <jrquick@uab.edu>
2019-10-29 22:21:44 -0500

    spacing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TooltipEvent} from '../../../projects/ionic-tooltips/src/models/tooltip-event.model';
 
 @Component({
-  selector: 'app-home',
+  selector:    'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: [
+    'home.page.scss'
+  ],
 })
 export class HomePage {
   tooltipEvent:TooltipEvent = TooltipEvent.CLICK;
