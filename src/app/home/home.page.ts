8401c2a1f9490482bb4ee2805d2f6755df2f459a
Jeremy Quick <jrquick@uab.edu>
2019-10-29 22:20:59 -0500

    Create enum

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TooltipEvent} from '../../../projects/ionic-tooltips/src/models/tooltip-event.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  tooltipEvent: 'click' | 'press' = 'click';
+  tooltipEvent:TooltipEvent = TooltipEvent.CLICK;
   showArrow: boolean = true;
   duration: number = 3000;
 }
